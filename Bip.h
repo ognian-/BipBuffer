5765e3e5a9f574a06924a1b189a27da9b7708539
Ognian Milanov <ognian@mokriya.com>
2017-03-02 18:36:28 +0200

    Added element type template; Removed thread-safe version for now

--- Bip.h
+++ Bip.h
@@ -8,350 +8,214 @@
 #include <cstdint>
 #include <cstring>
 
-#ifdef BIP_HAVE_THREAD
-
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-
-#endif // BIP_HAVE_THREAD
-
 namespace bip {
 
 namespace internal {
 
+template <typename T>
 struct Partition {
-    inline Partition(uint8_t* const * l, const uint8_t* const * u);
-    inline void put(const uint8_t* d, std::size_t s);
-    inline void get(uint8_t* d, std::size_t s);
-    inline void skip(std::size_t s);
-    inline std::size_t avail() const;
-    inline std::size_t free() const;
-    inline void reset();
-    uint8_t* begin;
-    uint8_t* end;
+    inline Partition(T* const* lower, const T* const* upper) noexcept;
+    inline void put(const T* data, std::size_t size) noexcept;
+    inline void get(T* data, std::size_t size) noexcept;
+    inline void skip(std::size_t size) noexcept;
+    inline std::size_t avail() const noexcept;
+    inline std::size_t free() const noexcept;
+    inline void reset() noexcept;
+    T* begin;
+    T* end;
 private:
-    uint8_t* const * lower;
-    const uint8_t* const * upper;
+    T* const* m_lower;
+    const T* const* m_upper;
 }; // struct Partition
 
 } // namespace internal
 
+template <typename T>
 class BIP {
 public:
     /*
-     * Construct a BIP buffer at memory block 'buf', with total size 's'
+     * Construct a BIP buffer at memory block 'buf', with total elements count 'size'
      */
-    BIP(uint8_t* buf, std::size_t s) :
-            lower(buf),
-            upper(buf + s),
-            A(&lower, &B.begin),
-            B(&A.end, &upper),
-            Get(&B),
-            Put(&B),
-            NextGet(&B),
-            NextPut(&B),
-            cons(false) {
-    }
+    BIP(T* buf, std::size_t size) noexcept;
 
     /*
-     * Attempt to write 's' bytes from 'd'. Returns actual bytes written
+     * Attempt to write 'size' elements from 'data'. Returns the count of actual elements written
      */
-    std::size_t put(const uint8_t* d, std::size_t s) {
-        const std::size_t f = free();
-        if (s >= f) {
-            Put->put(d, f);
-            NextGet = Put;
-            Put = NextPut;
-            return f;
-        }
-        Put->put(d, s);
-        return s;
-    }
+    std::size_t put(const T* data, std::size_t size) noexcept;
 
     /*
-     * Attempt to read 's' bytes into 'd'. Returns actual bytes read
+     * Attempt to read 'size' elements into 'data'. Returns the count of actual elements read
      */
-    std::size_t get(uint8_t* d, std::size_t s) {
-        const std::size_t a = avail();
-        if (s >= a) {
-            Get->get(d, a);
-            Get->reset();
-            NextPut = Get;
-            Get = NextGet;
-            return a;
-        }
-        Get->get(d, s);
-        return s;
-    }
+    std::size_t get(T* data, std::size_t size) noexcept;
 
     /*
-     * Attempt to skip 's' bytes. Returns actual bytes skipped
+     * Attempt to skip 'size' elements. Returns the count of actual elements skipped
      */
-    inline std::size_t skip(std::size_t s) {
-        const std::size_t a = avail();
-        if (s >= a) {
-            Get->skip(a);
-            Get->reset();
-            NextPut = Get;
-            Get = NextGet;
-            return a;
-        }
-        Get->skip(s);
-        return s;
-    }
+    inline std::size_t skip(std::size_t size) noexcept;
 
     /*
-     * Returns how many bytes are available for a single read
+     * Returns how many elements are available for a single read
      */
-    inline std::size_t avail() const {
-        return Get->avail();
-    }
+    inline std::size_t avail() const noexcept;
 
     /*
-     * Returns how many bytes can be written at most
+     * Returns how many elements can be written in a single write
      */
-    inline std::size_t free() const {
-        return Put->free();
-    }
+    inline std::size_t free() const noexcept;
 
     /*
-     * Returns true if there are no bytes available for read
+     * Returns true if there are no elements available for read
      */
-    inline bool empty() const {
-        return avail() == 0;
-    }
+    inline bool empty() const noexcept;
 
     /*
-     * Returns true if the buffer can't accept more bytes
+     * Returns true if the buffer can't accept more elements
      */
-    inline bool full() const {
-        return free() == 0;
-    }
+    inline bool full() const noexcept;
 
     /*
-     * Returns true if there are any bytes to be read
+     * Returns true if there are any elements to be read
      */
-    inline bool have() const {
-        return !empty();
-    }
-
-    /*
-     * Returns true if the input is marked as consumed
-     */
-    inline bool consumed() const {
-        return cons;
-    }
-
-    /*
-     * Marks the input as consumed
-     */
-    inline void setConsumed() {
-        cons = true;
-    }
+    inline bool have() const noexcept;
 
 private:
 
-    uint8_t* const lower;
-    const uint8_t* const upper;
-    internal::Partition A;
-    internal::Partition B;
-    internal::Partition* Get;
-    internal::Partition* Put;
-    internal::Partition* NextGet;
-    internal::Partition* NextPut;
-    bool cons;
+    T* const lower;
+    const T* const upper;
+    internal::Partition<T> A;
+    internal::Partition<T> B;
+    internal::Partition<T>* Get;
+    internal::Partition<T>* Put;
+    internal::Partition<T>* NextGet;
+    internal::Partition<T>* NextPut;
 }; // class BIP
-
-#ifdef BIP_HAVE_THREAD
-
-class LockedBIP {
-public:
-    /*
-     * Construct a BIP buffer at memory block 'buf', with total size 's'
-     */
-    LockedBIP(uint8_t* buf, std::size_t s) : bip(buf, s) {
-    }
-
-    /*
-     * Attempt to write 's' bytes from 'd'. Returns actual bytes written
-     */
-    std::size_t put(const uint8_t* d, std::size_t s) {
-        std::unique_lock<std::mutex> guard(mutex);
-        while (bip.full()) notFull.wait(guard);
-        std::size_t r = bip.put(d, s);
-        notEmpty.notify_one();
-        return r;
-    }
-
-    /*
-     * Attempt to read 's' bytes into 'd'. Returns actual bytes read
-     */
-    std::size_t get(uint8_t* d, std::size_t s) {
-        std::unique_lock<std::mutex> guard(mutex);
-        while (bip.empty() && !bip.consumed()) notEmpty.wait(guard);
-        std::size_t r = bip.get(d, s);
-        notFull.notify_one();
-        return r;
-    }
-
-    /*
-     * Attempt to skip 's' bytes. Returns actual bytes skipped
-     */
-    std::size_t skip(std::size_t s) {
-        std::unique_lock<std::mutex> guard(mutex);
-        while (bip.empty() && !bip.consumed()) notEmpty.wait(guard);
-        std::size_t r = bip.skip(s);
-        notFull.notify_one();
-        return r;
-    }
-
-    /*
-     * Block until all 's' bytes are written
-     */
-    std::size_t put_all(const uint8_t* d, std::size_t s) {
-        std::unique_lock<std::mutex> guard(mutex);
-        std::size_t all = 0;
-        while (all < s) {
-            while (bip.full()) notFull.wait(guard);
-            all += bip.put(d + all, s - all);
-        }
-        notEmpty.notify_one();
-        return all;
-    }
-
-    /*
-     * Block until all 's' bytes are read, or the stream is consumed
-     */
-    std::size_t get_all(uint8_t* d, std::size_t s) {
-        std::unique_lock<std::mutex> guard(mutex);
-        std::size_t all = 0;
-        while (all < s) {
-            while (bip.empty() && !bip.consumed()) notEmpty.wait(guard);
-            if (bip.consumed()) break;
-            all += bip.get(d + all, s - all);
-        }
-        notFull.notify_one();
-        return all;
-    }
-
-    /*
-     * Block until all 's' bytes are skipped, or the stream is consumed
-     */
-    std::size_t skip_all(std::size_t s) {
-        std::unique_lock<std::mutex> guard(mutex);
-
-        while (bip.empty() && !bip.consumed()) notEmpty.wait(guard);
-        std::size_t r = bip.skip(s);
-        notFull.notify_one();
-
-        return r;
-    }
-
-    /*
-     * Returns how many bytes are available for a single read
-     */
-    inline std::size_t avail() const {
-        std::unique_lock<std::mutex> guard(mutex);
-        return bip.avail();
-    }
-
-    /*
-     * Returns how many bytes can be written at most
-     */
-    inline std::size_t free() const {
-        std::unique_lock<std::mutex> guard(mutex);
-        return bip.free();
-    }
-
-    /*
-     * Returns true if there are no bytes available for read
-     */
-    inline bool empty() const {
-        std::unique_lock<std::mutex> guard(mutex);
-        return bip.empty();
-    }
-
-    /*
-     * Returns true if the buffer can't accept more bytes
-     */
-    inline bool full() const {
-        std::unique_lock<std::mutex> guard(mutex);
-        return bip.full();
-    }
-
-    /*
-     * Returns true if there are any bytes to be read
-     */
-    inline bool have() const {
-        std::unique_lock<std::mutex> guard(mutex);
-        return bip.have();
-    }
-
-    /*
-     * Returns true if the input is marked as consumed
-     */
-    inline bool consumed() const {
-        std::unique_lock<std::mutex> guard(mutex);
-        return bip.consumed();
-    }
-
-    /*
-     * Marks the input as consumed
-     */
-    inline void setConsumed() {
-        std::unique_lock<std::mutex> guard(mutex);
-        bip.setConsumed();
-        notEmpty.notify_all();
-    }
-
-private:
-    BIP bip;
-    mutable std::mutex mutex;
-    std::condition_variable notFull;
-    std::condition_variable notEmpty;
-
-}; // class LockedBIP
-
-#endif // BIP_HAVE_THREAD
 
 } // namespace bip
 
 namespace bip {
+
+template <typename T>
+BIP<T>::BIP(T* buf, std::size_t size) noexcept :
+		lower{buf},
+		upper{buf + size},
+		A{&lower, &B.begin},
+		B{&A.end, &upper},
+		Get{&B},
+		Put{&B},
+		NextGet{&B},
+		NextPut{&B} {
+}
+
+template <typename T>
+std::size_t BIP<T>::put(const T* data, std::size_t size) noexcept {
+	const auto f = free();
+	if (size >= f) {
+		Put->put(data, f);
+		NextGet = Put;
+		Put = NextPut;
+		return f;
+	}
+	Put->put(data, size);
+	return size;
+}
+
+template <typename T>
+std::size_t BIP<T>::get(T* data, std::size_t size) noexcept {
+	const auto a = avail();
+	if (size >= a) {
+		Get->get(data, a);
+		Get->reset();
+		NextPut = Get;
+		Get = NextGet;
+		return a;
+	}
+	Get->get(data, size);
+	return size;
+}
+
+template <typename T>
+std::size_t BIP<T>::skip(std::size_t size) noexcept {
+	const auto a = avail();
+	if (size >= a) {
+		Get->skip(a);
+		Get->reset();
+		NextPut = Get;
+		Get = NextGet;
+		return a;
+	}
+	Get->skip(size);
+	return size;
+}
+
+template <typename T>
+std::size_t BIP<T>::avail() const noexcept {
+	return Get->avail();
+}
+
+template <typename T>
+std::size_t BIP<T>::free() const noexcept {
+	return Put->free();
+}
+
+template <typename T>
+bool BIP<T>::empty() const noexcept {
+	return avail() == 0;
+}
+
+template <typename T>
+bool BIP<T>::full() const noexcept {
+	return free() == 0;
+}
+
+template <typename T>
+bool BIP<T>::have() const noexcept {
+	return !empty();
+}
+
 namespace internal {
 
-Partition::Partition(uint8_t* const * l, const uint8_t* const * u) : lower(l), upper(u) {
+template <typename T>
+Partition<T>::Partition(T* const* lower, const T* const* upper) noexcept :
+		begin{},
+		end{},
+		m_lower{lower},
+		m_upper{upper} {
     reset();
 }
 
-void Partition::put(const uint8_t* d, std::size_t s) {
-    memcpy(end, d, s);
-    end += s;
+template <typename T>
+void Partition<T>::put(const T* data, std::size_t size) noexcept {
+    memcpy(end, data, size * sizeof(T));
+    end += size;
 }
 
-void Partition::get(uint8_t* d, std::size_t s) {
-    memcpy(d, begin, s);
-    begin += s;
+template <typename T>
+void Partition<T>::get(T* data, std::size_t size) noexcept {
+    memcpy(data, begin, size * sizeof(T));
+    begin += size;
 }
 
-void Partition::skip(std::size_t s) {
-    begin += s;
+template <typename T>
+void Partition<T>::skip(std::size_t size) noexcept {
+    begin += size;
 }
 
-std::size_t Partition::avail() const {
+template <typename T>
+std::size_t Partition<T>::avail() const noexcept {
     return end - begin;
 }
 
-std::size_t Partition::free() const {
-    return *upper - end;
+template <typename T>
+std::size_t Partition<T>::free() const noexcept {
+    return *m_upper - end;
 }
 
-void Partition::reset() {
-    begin = end = *lower;
+template <typename T>
+void Partition<T>::reset() noexcept {
+    begin = end = *m_lower;
 }
 
 } // namespace internal
 } // namespace bip
 
 #endif // BIP_H_INCLUDED
-
